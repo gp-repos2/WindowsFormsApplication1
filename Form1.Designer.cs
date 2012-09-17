6c518489549db4c7f45dc9e068d09b22eb66f1d0
Paul G <f9cgnynls7xg@mail.ru>
2012-09-17 15:46:41 +0400

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axSpreadsheet1 = new AxMicrosoft.Office.Interop.Owc11.AxSpreadsheet();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axSpreadsheet1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axSpreadsheet1
+            // 
+            this.axSpreadsheet1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.axSpreadsheet1.DataSource = null;
+            this.axSpreadsheet1.Enabled = true;
+            this.axSpreadsheet1.Location = new System.Drawing.Point(0, 60);
+            this.axSpreadsheet1.Name = "axSpreadsheet1";
+            this.axSpreadsheet1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSpreadsheet1.OcxState")));
+            this.axSpreadsheet1.Size = new System.Drawing.Size(696, 424);
+            this.axSpreadsheet1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 54);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(0, 490);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(696, 112);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 602);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.axSpreadsheet1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axSpreadsheet1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMicrosoft.Office.Interop.Owc11.AxSpreadsheet axSpreadsheet1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Reflection;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+using Owc11 = Microsoft.Office.Interop.Owc11;
+
+namespace WindowsFormsApplication1
+{
+ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlBook;
+            Excel.Worksheet xlSheet1, xlSheet2, xlSheet3;
+
+            xlApp = new Excel.Application();
+            xlBook = xlApp.Workbooks.Open("C:\\Documents and Settings\\PGustov\\Desktop\\Книга1.xls", 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            //xlApp.Visible = true;
+            //xlApp.Parent.Windows[1].Visible = true;
+
+
+
+            while (axSpreadsheet1.Sheets.Count > xlBook.Worksheets.Count)
+            {
+//                Owc11.Worksheet owcSheet2 = (Owc11.Worksheet)axSpreadsheet1.Sheets[axSpreadsheet1.Sheets.Count];
+//                MessageBox.Show(axSpreadsheet1.ActiveSheet.Name);
+                try
+                {
+                    axSpreadsheet1.ActiveSheet.Delete();
+                }
+                catch (Exception eee)
+                {
+                    MessageBox.Show(eee.ToString());
+                }
+            }
+
+                       
+            if (xlBook.Worksheets.Count > 3)
+            {
+                axSpreadsheet1.Sheets.Add(Type.Missing, Type.Missing, xlBook.Worksheets.Count - 3, Type.Missing);
+            }
+
+            int i=0;
+            Owc11.Worksheet owcSheet;
+
+            foreach (Excel.Worksheet xlSheet in xlBook.Worksheets)
+            {
+                i++;
+                owcSheet = (Owc11.Worksheet)axSpreadsheet1.Sheets[i];
+                owcSheet.Name = "szxcxcx" + i.ToString();
+            }
+
+            i = 0;
+            foreach (Excel.Worksheet xlSheet in xlBook.Worksheets)
+            {
+                //MessageBox.Show(xlSheet.Name);
+
+                i++;
+                 xlSheet.Activate();
+                 xlSheet.Cells.Select();
+                 xlSheet.Cells.Copy(Type.Missing);
+
+                 //owcSheet = (Owc11.Worksheet)axSpreadsheet1.Sheets.Add(axSpreadsheet1.Sheets[1], null, 1, Type.Missing);
+                 
+                 owcSheet=(Owc11.Worksheet)axSpreadsheet1.Sheets[i];
+
+                 owcSheet.Name = xlSheet.Name;
+                 owcSheet.Activate();
+                 owcSheet.Cells.Select();
+                 owcSheet.Cells.Paste();
+            }
+
+
+            xlApp.DisplayAlerts = false;
+            xlBook.Close(Type.Missing, Type.Missing, Type.Missing);          
+            xlApp.Quit();
+            
+            
+/*            if (axSpreadsheet1.Sheets.Count > 1)
+            {
+                try
+                {
+                    ((Owc11.Worksheet)axSpreadsheet1.Sheets[1]).Delete();
+                }
+                catch (Exception eee)
+                {
+                    MessageBox.Show(eee.ToString());
+                }
+            }
+*/
+
+
+/*
+Dim objexcel As Object
+    ' Dim objworkbook As Object
+    Dim objsheet As Object
+
+    ' Dim objExcel As New Excel.ApplicationClass
+    ' Dim objSheet As Excel.Worksheet
+    Try
+      'Create and open the correct excel workbook
+      objExcel = CreateObject("Excel.Application")
+      objexcel.Workbooks.Open("C:\Central systems\Central systems\Folders\Department Log\Reports\Queries Report 2010.xls")
+      objExcel.Visible = True
+      objExcel.Parent.Windows(1).Visible = True
+    Catch ex As Exception
+      MessageBox.Show(ex.Message)
+    End Try
+
+    'Select all the cells in the relevant worksheet
+    'and copy the contents
+    objsheet = objexcel.ActiveSheet
+    objsheet = objexcel.Sheet("Jan" & Now.Year)
+    objSheet.Cells.Select()
+    objSheet.Cells.Copy()
+
+    'Select a cell in the AxSpreadsheet control that is 
+    'already declared and initialised in the class
+    Me.AxSpreadsheet1.Cells.Range("A1:Z5000").Select()
+
+    'Paste the contents
+    Me.AxSpreadsheet1.Cells.Range("A1").Paste()
+    Me.AxSpreadsheet1.Sheets.Add(12)
+*/
+        }
+    }
+}
